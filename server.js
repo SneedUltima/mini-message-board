6b647e180779ca0256bca5cceb6221b329b976c4
Dylan <dylan.cobham@yahoo.com.au>
2022-12-01 16:13:56 +1030

    build: add ejs render

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const port = 3000;
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
@@ -11,9 +13,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
 });
 
 app.use((req, res) => {
-  res.status(404).res.sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404");
 });
